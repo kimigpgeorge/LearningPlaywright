45e61e0580405f4df31c1bd42e0e7a3fb2604fb4
kimigpgeorge <kimigpgeorge@gmail.com>
2026-02-25 21:58:30 +0530

    Add JavaScript learning materials and practice files

---
+++ pratice/gradecal.js
@@ -0,0 +1,24 @@
+//grade calculation
+let g =82;
+let A,b,c,d,e,f;
+if ((g>=90)&&(g<=100))
+
+{
+	console.log("grade = a");
+
+}else if ((g>=80)&&(g<=89))
+{
+	
+	console.log("grade = b");
+}else if ((g>=70)&&(g<=79))
+{
+	
+	console.log("grade = c");
+}else if ((g>=60)&&(g<=69))
+{
+	
+	console.log("grade = d");
+}else 
+{
+	console.lof("grade f")
+}
