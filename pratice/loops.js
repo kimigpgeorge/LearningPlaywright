10a6bc85f620d49fd18fdc4e289456b38f3297c7
kimigpgeorge <kimigpgeorge@gmail.com>
2026-02-27 21:54:46 +0530

    Update learning materials

---
+++ pratice/loops.js
@@ -0,0 +1,46 @@
+console.log("forloop");
+
+//Print "Hello" 5 times
+
+for(let i=0; i<5; i++)
+{
+    console.log("Hello");
+
+
+}
+//Print numbers from 1 to 10
+console.log("****");
+
+for(let k=0; k<=10; k++)
+    {
+    console.log(k);
+}
+//Print even numbers from 1 to 20
+console.log("****");
+for(let e=1; e<=20; e++)
+{
+    if(e%2==0)
+    {
+        console.log(e);
+
+    }
+}
+console.log("****");
+// Calculate sum of first 10 natural numbers
+let num=0;
+for(let n=0; n<=10; n++)
+{
+    num=num+n;
+}
+console.log(num);
+
+console.log("%%%");
+// Print multiplication table of 5
+let mul=0
+for(let l=1; l<=10; l++)
+{
+   console.log(l*5);
+}
+
+   
+
